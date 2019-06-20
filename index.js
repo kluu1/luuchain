4f799df0cc6f9b6abb96a6aa3d5f8c5a68e01d57
Kevin Luu <kluu@epictech.cm>
2019-06-19 21:57:59 -0400

    pub and sub finished

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const Blockchain = require('./blockchain');
+const PubSub = require('./pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+setTimeout(() => {
+  pubsub.broadcastChain();
+}, 1000); 
 
 app.use(bodyParser.json());
 
